0ca6da1c7ddad2c5650bec2f63d6bb44e0bbf9a4
enes-erenn <thenesern@gmail.com>
2022-12-08 17:38:09 +0300

    Added Login API

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -2,6 +2,8 @@ import { Request, Response } from "express";
 import { db } from "../db";
 import { MysqlError } from "mysql";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../types/types";
 
 export const register = (req: Request, res: Response): void => {
   // EXISTING USER CHECK
@@ -9,10 +11,12 @@ export const register = (req: Request, res: Response): void => {
   db.query(
     query,
     [req.body.email, req.body.username],
-    (err: MysqlError | null, data: any) => {
+    (err: MysqlError | null, data: User[]) => {
       if (err) return res.json(err);
 
-      if (data.length) return res.status(409).json("User already exists!");
+      if (data.length) {
+        return res.status(409).json("User already exists!");
+      }
 
       // HASH THE PASSWORD
       const salt = bcrypt.genSaltSync(10);
@@ -24,7 +28,7 @@ export const register = (req: Request, res: Response): void => {
 
       const values = [req.body.username, req.body.email, hashedPassword];
 
-      db.query(query, [values], (err: MysqlError | null, data: any) => {
+      db.query(query, [values], (err: MysqlError | null, data: User[]) => {
         if (err) return res.json(err);
 
         return res.status(200).json("User has been created successfully");
@@ -32,9 +36,32 @@ export const register = (req: Request, res: Response): void => {
     }
   );
 };
-
 export const login = (req: Request, res: Response): void => {
-  res.json();
+  // EXISTING USER CHECK
+  const query = "SELECT * FROM users WHERE email = ?";
+
+  db.query(query, [req.body.email], (err: MysqlError | null, data: User[]) => {
+    if (err) return res.json(err);
+
+    if (data.length === 0) return res.status(404).json("User not found!");
+
+    // PASSWORD CHECK
+    const isPasswordCorrect = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!isPasswordCorrect)
+      return res.status(400).json("Wrong email or password!");
+
+    const token = jwt.sign({ id: data[0].id }, "auth_jwt_key");
+
+    const { password, ...other } = data[0];
+
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(other);
+  });
 };
 
 export const logout = (req: Request, res: Response): void => {
--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import userRoutes from "./routes/users";
 import authRoutes from "./routes/auth";
 import dotenv from "dotenv";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app: Express = express();
@@ -13,7 +14,9 @@ app.use(
     origin: "http://localhost:3000",
   })
 );
+
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/auth", authRoutes);
